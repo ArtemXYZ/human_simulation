437b5b7281c7ee0eaf232db5fa1d53cdbea8296b
Artem <poznyshev.a.a@yandex.ru>
2024-09-15 18:40:43 +0400

    + Базовый Нейрон. Начало. Простейшее решение.

---
+++ an_simulation/core/mental_wrapper/brain_module/output_data.py
@@ -0,0 +1,38 @@
+"""
+Класс содержит методы имитирующие первичную единицу головного мозга - нейрон, состоящий из дендритов и одного аксона.
+"""
+
+
+class Data:
+
+    def __init__(self):
+        pass
+
+
+class Synapse:
+
+    def __init__(self, data):
+
+
+        self.data = data  # 1.
+
+
+#   будет принимать и дендриты и аксоны
+
+
+class Dendrite:
+
+    def __init__(self):
+        pass
+
+    def get_info_data_in_output(self):
+        pass
+
+
+class Axon:
+
+    def __init__(self):
+        pass
+
+    def get_info_data_in_intput(self):
+        pass
